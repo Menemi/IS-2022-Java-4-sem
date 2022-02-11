024d7b3d87301b979cc121723d1bc58b611b2fff
1Menemi1 <menemi@niuitmo.ru>
2022-02-12 01:39:43 +0300

    feat: add class 4 work with deposit percents

---
+++ banks-java/src/main/java/ru/itmo/banks/PercentOfTheAmount.java
@@ -0,0 +1,26 @@
+package ru.itmo.banks;
+
+public class PercentOfTheAmount {
+    public PercentOfTheAmount(double lowerBound, double upperBound, double percent)
+    {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        this.percent = percent;
+    }
+
+    public double lowerBound;
+    public double upperBound;
+    public double percent;
+
+    public double getLowerBound() {
+        return lowerBound;
+    }
+
+    public double getUpperBound() {
+        return upperBound;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+}
