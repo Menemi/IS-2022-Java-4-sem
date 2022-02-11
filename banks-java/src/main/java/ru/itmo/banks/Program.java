4fdd01320e04a179bd3094cac4f923861ed576c9
1Menemi1 <menemi@niuitmo.ru>
2022-02-12 01:40:03 +0300

    feat: add main-program class

---
+++ banks-java/src/main/java/ru/itmo/banks/Program.java
@@ -0,0 +1,11 @@
+package ru.itmo.banks;
+
+import ru.itmo.banks.Accounts.Account;
+import ru.itmo.banks.Exceptions.BanksException;
+
+import java.time.LocalDate;
+
+public class Program {
+    public static void main(String[] args) throws BanksException {
+    }
+}
