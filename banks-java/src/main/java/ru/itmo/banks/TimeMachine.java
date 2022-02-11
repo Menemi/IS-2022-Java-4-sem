31ad628865329a66792754d6b3f5aacb406810a6
1Menemi1 <menemi@niuitmo.ru>
2022-02-12 01:39:50 +0300

    feat: add time machine

---
+++ banks-java/src/main/java/ru/itmo/banks/TimeMachine.java
@@ -0,0 +1,12 @@
+package ru.itmo.banks;
+
+import ru.itmo.banks.Banks.CentralBank;
+
+import java.time.LocalDate;
+
+public class TimeMachine {
+    public void TimeRewind(CentralBank centralBank, LocalDate dateToRewind)
+    {
+        centralBank.notifyObservers(dateToRewind);
+    }
+}
