d2330ccf3d17d817c049cc67fd4b6eab0136d0f1
1Menemi1 <menemi@niuitmo.ru>
2022-04-08 17:10:11 +0300

    feat: add necessary annotation

--- kotiki-java/kotiki/src/main/java/ru/itmo/kotiki/KotikiApplication.java
+++ kotiki-java/kotiki/src/main/java/ru/itmo/kotiki/KotikiApplication.java
@@ -2,10 +2,12 @@ package ru.itmo.kotiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"ru.itmo.kotiki.model", "ru.itmo.kotiki.service", "ru.itmo.kotiki.controller"})
 public class KotikiApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(KotikiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KotikiApplication.class, args);
+    }
 }
