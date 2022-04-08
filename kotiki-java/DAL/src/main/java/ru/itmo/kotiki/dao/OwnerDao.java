0c7fc95307a71c72ff4be02a1a454162ea4ab9a5
1Menemi1 <menemi@niuitmo.ru>
2022-04-08 17:09:20 +0300

    refactor: change dao to JpaRepository

--- kotiki-java/DAL/src/main/java/ru/itmo/kotiki/dao/OwnerDao.java
+++ kotiki-java/DAL/src/main/java/ru/itmo/kotiki/dao/OwnerDao.java
@@ -1,20 +1,9 @@
 package ru.itmo.kotiki.dao;
 
-import ru.itmo.kotiki.model.Cat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.itmo.kotiki.model.Owner;
 
-import java.util.List;
-
-public interface OwnerDao {
-    public Owner findById(int id);
-
-    public void save(Owner owner);
-
-    public void update(Owner owner);
-
-    public void delete(Owner owner);
-
-    public Cat findCatById(int id);
-
-    public List<Owner> findAll();
+@Repository
+public interface OwnerDao extends JpaRepository<Owner, Long> {
 }
