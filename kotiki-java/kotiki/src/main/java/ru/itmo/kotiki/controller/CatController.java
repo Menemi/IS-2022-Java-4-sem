b2d25b9cd4df9f0148faa4c5916364bcf5d304e9
1Menemi1 <47279903+1Menemi1@users.noreply.github.com>
2022-04-23 21:36:25 +0300

    feat: add secret access 4 simple users

--- kotiki-java/kotiki/src/main/java/ru/itmo/kotiki/controller/CatController.java
+++ kotiki-java/kotiki/src/main/java/ru/itmo/kotiki/controller/CatController.java
@@ -3,15 +3,17 @@ package ru.itmo.kotiki.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import ru.itmo.kotiki.Generator;
 import ru.itmo.kotiki.dto.CatDto;
-import ru.itmo.kotiki.model.Owner;
-import ru.itmo.kotiki.service.CatServiceImpl;
 import ru.itmo.kotiki.model.Cat;
+import ru.itmo.kotiki.service.CatService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/cats")
@@ -22,21 +24,39 @@ public class CatController {
     // put / patch - изменяет,
     // delete - удаляет
 
-    @Autowired
-    private CatServiceImpl catService;
-
     private final Generator generator = new Generator();
+    @Autowired
+    private CatService catService;
 
     @GetMapping("/get/{id}")
     public CatDto getCatById(@PathVariable int id) {
-        return generator.catToCatDto(catService.findCat(id));
+        Cat cat = catService.findCat(id);
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails) principal).getUsername();
+
+        if (username.equals("admin") || Objects.equals(catService.findCat(id).getOwner().getName(), username)) {
+            return generator.catToCatDto(cat);
+        }
+
+        return new CatDto();
     }
 
     @GetMapping("/get")
     public List<CatDto> getCats() {
         List<CatDto> catsDto = new ArrayList<>();
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails) principal).getUsername();
+
         for (Cat cat : catService.findAllCats()) {
-            catsDto.add(generator.catToCatDto(cat));
+            if (cat.getOwner() == null && !username.equals("admin")) {
+                continue;
+            }
+
+            if (username.equals("admin") || Objects.equals(cat.getOwner().getName(), username)) {
+                catsDto.add(generator.catToCatDto(cat));
+            }
         }
 
         return catsDto;
@@ -44,20 +64,38 @@ public class CatController {
 
     @PostMapping("/create")
     public ResponseEntity<?> createCat(@RequestBody CatDto catDto) {
-        Cat cat = generator.dtoCatToCat(catDto);
-        catService.saveCat(cat);
-        return new ResponseEntity<>(HttpStatus.OK);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails) principal).getUsername();
+
+        if (username.equals("admin")) {
+            Cat cat = generator.dtoCatToCat(catDto);
+            catService.saveCat(cat);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
     }
 
     @PutMapping("/update/{id}")
     public void updateCat(@PathVariable int id, String name) {
         Cat cat = catService.findCat(id);
-        cat.setName(name);
-        catService.saveCat(cat);
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails) principal).getUsername();
+        if (username.equals("admin") || Objects.equals(catService.findCat(id).getOwner().getName(), username)) {
+            cat.setName(name);
+            catService.saveCat(cat);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteCat(@PathVariable int id) {
-        catService.deleteCat(catService.findCat(id));
+        Cat cat = catService.findCat(id);
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails) principal).getUsername();
+        if (username.equals("admin")) {
+            catService.deleteCat(cat);
+        }
     }
 }
